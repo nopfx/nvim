05c09642b554eb18b2969753d9cc17b6a4892be2
Nop Fx <nopfx@Nops-MacBook-Air.local>
2025-08-25 10:19:31 +0300

    Spaces and other stuff

--- lua/plugins/formatters.lua
+++ lua/plugins/formatters.lua
@@ -4,6 +4,13 @@ return {
 	config = function()
 		local conform = require("conform")
 
+		-- Treat .htnl as html
+		vim.filetype.add({
+			extension = {
+				htnl = "html",
+			},
+		})
+
 		conform.setup({
 			format_on_save = {
 				timeout_ms = 500,
@@ -11,18 +18,50 @@ return {
 				async = false,
 			},
 			formatters_by_ft = {
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				css = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
+				javascript = { "prettier_js" },
+				typescript = { "prettier_js" },
+				css = { "prettier_css" },
+				html = { "prettier_htnl", "prettier" }, -- try htnl-aware first
+				json = { "prettier_js" },
 				yaml = { "prettier" },
 				markdown = { "prettier" },
-				javascriptreact = { "prettier" },
+				javascriptreact = { "prettier_js" },
 				rust = { "rustfmt" },
 				go = { "gofumpt", "goimports" },
 				lua = { "stylua" },
 				python = { "isort", "black" },
+			},
+			formatters = {
+				prettier_htnl = {
+					command = "prettier",
+					args = function(ctx)
+						local fname = ctx.filename or "stdin.html"
+						local base_args = {
+							"--stdin-filepath",
+							fname,
+							"--tab-width",
+							"4",
+							"--use-tabs",
+							"false",
+						}
+						if fname:match("%.htnl$") then
+							table.insert(base_args, "--parser")
+							table.insert(base_args, "html")
+						end
+						return base_args
+					end,
+					stdin = true,
+				},
+				prettier_css = {
+					command = "prettier",
+					args = { "--stdin-filepath", "stdin.css", "--tab-width", "4", "--use-tabs", "false" },
+					stdin = true,
+				},
+				prettier_js = {
+					command = "prettier",
+					args = { "--stdin-filepath", "stdin.js", "--tab-width", "4", "--use-tabs", "false" },
+					stdin = true,
+				},
 			},
 		})
 	end,
